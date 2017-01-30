97d9d081dad98cb78e9b49af60dd80372ea50efa
mkunaparaju <mkunaparaju@gmail.com>
2017-01-30 14:43:45 -0500

    icream problem solved,but recheck. not sure about impl

--- IceCreamParlour.java
+++ IceCreamParlour.java
@@ -8,21 +8,23 @@ import java.util.regex.*;
  * @author mkunaparaju
  *
  */
-class IceCream implements Comparable{
+class IceCream implements Comparable<IceCream>{
     int flavor;
     int index;
 
     public IceCream(int flavor, int index) {
-      
+    	this.flavor = flavor;
+    	this.index = index;
     }
    
     @Override
-    public int compareTo(Object o) {
-       
+    public int compareTo(IceCream i) {
+    	return (int)(this.flavor-i.flavor);
     }
 
     @Override
-    public boolean equals(Object o){
+    public boolean equals(Object i){
+        return (this.flavor == (int)i ? true:false);
        
     }
         
@@ -31,8 +33,35 @@ class IceCream implements Comparable{
 public class IceCreamParlour {
    
     public static int binarySearch(int first, int last, IceCream[] arr, int search) {
-        
-        
+//    	if(first > last)	{
+//    		return -1;
+//    	}
+//    	int mid = (first + last) / 2;
+//    	if(arr[mid].equals(search))	{
+//    		return mid;
+//    	}
+//    	if(search < arr[mid].flavor)	{
+//    		return binarySearch(first, mid - 1, arr, search);
+//    	}
+//    	if(search > arr[mid].flavor)	{
+//    		return binarySearch(mid + 1, last, arr, search);
+//    	}  	
+//    	return 1;
+    	
+    	 if(first<=last){
+             int mid=(first+last)/2;
+             if(arr[mid].flavor == search){
+                return arr[mid].index; 
+             } 
+             else if (arr[mid].flavor > search){
+                 return binarySearch(first,mid-1,arr,search);
+             }
+             else {
+                 return binarySearch(mid+1,last,arr,search);
+             }
+         }
+         else return -1;
+         
     }
 
     public static void main(String[] args) {
