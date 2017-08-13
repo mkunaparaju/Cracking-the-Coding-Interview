bbfff84c2d9ebf2551d841c42fc5f202f2c56e59
mkunaparaju <mkunaparaju@gmail.com>
2017-08-13 18:55:47 -0500

    completed build order example

--- TextTreesGraphs/BuildOrder.java
+++ TextTreesGraphs/BuildOrder.java
@@ -18,16 +18,62 @@ public class BuildOrder {
 		
 	}
 	
-	Project[] getBuildOrderString [] projects, String [][] dependencies)	{
-		
+	Project[] getBuildOrder(String [] projects, String [][] dependencies)	{
+		OrderGraph graph = buildGraph(projects, dependencies);
+		Project [] projectList = orderProjects(graph.getProjectList());
+		return projectList;
 	}
 	
-	Graph buildGraph(String [] projects, String [][] dependencies)	{
+	OrderGraph buildGraph(String [] projects, String [][] dependencies)	{
 		
+		OrderGraph graph = new OrderGraph();
+		
+		for(String project : projects)	{
+			graph.getAddProject(project);
+		}
+		
+		for(String [] dependency : dependencies)	{
+			String start	= dependency[0];
+			String end 		= dependency[1];
+			graph.addEdge(start, end);
+		}
+		
+		return graph;
 	}
 	
-	Project[] orderProjects(List<Project> projectList) {
+	Project[] orderProjects(ArrayList<Project> projectList) {
 		
+		Project[] order = new Project[projectList.size()];
+		int endOfList = addNonDependent(projectList, order, 0);
+		int toBeProcessed = 0;
+		
+		while(toBeProcessed < order.length)	{
+			Project current = order[toBeProcessed];
+			if(current == null) return null;
+			
+			// This removes current as a dependency 
+			ArrayList<Project> children = current.getChildren();
+			for(Project child : children)	{
+				child.decrement();
+			}
+			
+			// to order, add projects with no dependencies
+			endOfList = addNonDependent(children, order, endOfList);
+			toBeProcessed++;
+			
+		}
+		
+		return order;
+	}
+	
+	int addNonDependent(ArrayList<Project> projectList, Project[] ordered, int offset)	{
+		for( Project proj: projectList)	{
+			if(proj.getNumDependencies() == 0)	{
+				ordered[offset] = proj;
+				offset++;
+			}
+		}
+		return offset;
 	}
 
 }
@@ -37,7 +83,9 @@ class Project	{
 	
 	String name;
 	int dependencies = 0; // number of projects which depend on this project
+	
 	ArrayList<Project> children = new ArrayList<Project>();
+	
 	// Map of all the children to this project
 	// If it already has a project, the child is already mapped
 	HashMap<String, Project> childMap = new HashMap<String, Project>(); 
@@ -61,10 +109,17 @@ class Project	{
 	void addChildren(Project p )	{
 		if(!childMap.containsKey(p.getName()))	{
 			childMap.put(p.getName(), p);
-			dependencies++; 
+			p.increment(); 
 			children.add(p);
-			
 		}
+	}
+	
+	void increment() {
+		dependencies++;
+	}
+	
+	void decrement()	{
+		dependencies--;
 	}
 }
 
@@ -72,7 +127,7 @@ class Project	{
 
 class OrderGraph	{
 	
-	List<Project> projectList = new ArrayList<Project>();
+	ArrayList<Project> projectList = new ArrayList<Project>();
 	Map<String, Project> projectMap = new HashMap<String, Project>();
 	
 	public Project getAddProject(String name)	{
@@ -91,7 +146,7 @@ class OrderGraph	{
 		startProj.addChildren(endProj);
 	}
 	
-	public List<Project> getProjectList()	{
+	public ArrayList<Project> getProjectList()	{
 		return projectList;
 	}
 }
